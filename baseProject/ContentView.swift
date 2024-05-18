9d563c27b4124425d7f97ab00fe643c9c5824f67
rmenjivar <menjivarmenjivar.rimm@gmail.com>
2024-05-17 22:51:56 -0700

    Initial Commit

---
+++ baseProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  baseProject
+//
+//  Created by Rogelio on 5/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
